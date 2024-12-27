82830d888af6a439ac461fa7132cbbd5f6bbcf6b
cody harrison <charrison66@gatech.edu>
2024-12-27 13:26:35 -0600

    added some todo's and ideas for enhancements.

--- photo.py
+++ photo.py
@@ -2,11 +2,12 @@ import os
 import imagehash
 import shutil
 import pickle
-from PIL import Image
+from PIL import Image, ExifTags
 
 
 #PATHS
-
+#TODO: probably have it write these folders out into the project dir just so we can have everything working in one spot.
+# since rn it throws errors if you dont have the right folders
 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
 to_be_sorted_path = os.path.join(desktop_path, "tobesorted")
 deleted_path = os.path.join(desktop_path, "deleted")
@@ -41,18 +42,46 @@ for item in contents:
     if item.startswith('.'):
         continue
     try:
-            
         hash1 = imagehash.phash(Image.open(item_path))
         if hash1 in photo_id:
             # move photo into deleted dir
+            # TODO: check to see if there is a folder that has been created for deleted, clean etc. if not there make dir, currently throwing exception
             shutil.move(item_path, os.path.join(deleted_path, item))
         else:
             # move to clean dir and put hashed id into photo_id
+            # TODO: check to see if there is a folder that has been created for clean etc. if not there make dir, currently throwing exception
             photo_id[hash1]=0
             shutil.move(item_path, os.path.join(clean_path, item))
     except Exception as e:
         print(f"Error processing '{item}': {e}")
 
+    #TODO: open and read file to see if there is any meta data that we can extract.
+    img = Image.open(item_path)
+    exif = {ExifTags.TAGS[k]: v for k, v in img.getexif().items() if k in ExifTags.TAGS}
+    print(exif)
+
+    #TODO: process image and see if there is anything that you can visually extract from the image to understand what its about
+
+
+    #TODO: scan image off faces we know/have trained on
+    # open CV
+
+    #TODO: extract text from image
+    # open cv might be able to do something
+
+    #TODO: extract location data if available
+    # https://stackoverflow.com/questions/67037083/cant-get-gps-coordinates-from-from-exif-data-in-python-pillow-or-pyexiv2
+    # https://github.com/python-pillow/Pillow/issues/5863#issuecomment-984917141
+    # https://stackoverflow.com/questions/19804768/interpreting-gps-info-of-exif-data-from-photo-in-python
+
+    #TODO: process location data into country, city, etc
+
+
+    #TODO: save image hash and meta data into a database of some kind so that we can keep track of what we have processes
+
+
+    #TODO: on the app display a summary about the image/images.
+    # eg: camera, date, location, filename, display small portion of the file.
 
 
 with open(photo_id_path, "wb") as file:
